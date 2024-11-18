1cbcde08eacfba37092468aa0f9bc8770489c5c1
adil <adilsaid64@gmail.com>
2024-11-18 00:00:01 +0000

    feat: moved stramers to seperate docker container.

--- jobs/main.py
+++ jobs/main.py
@@ -3,7 +3,7 @@ from src.twitter_fetcher import TwitterFetcher
 from src.kafka_producer import KafkaProducer
 
 from src.context import  DataFetcher, MessageProducer
-
+from src.proj_logger import logger
 from dotenv import load_dotenv
 import os
 import json
@@ -12,7 +12,7 @@ import pandas as pd
 def stream_data(data_fetcher:DataFetcher, message_producer:MessageProducer):
     for submission in data_fetcher.fetch():
         message = json.dumps(submission)
-        print(message)
+        logger.info(message)
         message_producer.push(message)
 
 if __name__ == "__main__":
@@ -27,12 +27,11 @@ if __name__ == "__main__":
     #                                            reddit_user_agent, 
     #                                            subreddit))
     reddit_topic = 'reddit'
-    
-    twitter_fetcher = DataFetcher(TwitterFetcher(df = pd.read_csv('../data/twitter_dataset.csv')))
+    twitter_fetcher = DataFetcher(TwitterFetcher(df = pd.read_csv('data/twitter_dataset.csv')))
     twitter_topic = 'twitter'
 
-    kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092')
-    
+    # kafka_servers = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'broker:9092')
+    kafka_servers = 'broker:29092'
     kafka_producer = MessageProducer(KafkaProducer(kafka_server=kafka_servers, topic = twitter_topic))
 
     stream_data(data_fetcher=twitter_fetcher, 

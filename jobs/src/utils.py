90b17128238b40f3755f7abc7cd8a1e835c26a10
adil <adilsaid64@gmail.com>
2024-11-18 01:16:36 +0000

    feat: added optional sleep timer

--- jobs/src/utils.py
+++ jobs/src/utils.py
@@ -2,9 +2,11 @@ from src.proj_logger import logger
 from src.context import DataFetcher, MessageProducer
 
 import json
+import time
 
-def stream_data(data_fetcher:DataFetcher, message_producer:MessageProducer):
+def stream_data(data_fetcher:DataFetcher, message_producer:MessageProducer, sleep_time_s : int = 0):
     for submission in data_fetcher.fetch():
         message = json.dumps(submission)
         logger.info(message)
         message_producer.push(message)
+        if sleep_time_s>0:time.sleep(sleep_time_s)

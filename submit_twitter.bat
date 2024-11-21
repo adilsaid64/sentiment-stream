f487dd78f18d8a755fe3534e0a5639e27169eb6c
adil <adilsaid64@gmail.com>
2024-11-21 22:52:57 +0000

    chor: created submit spark job scripts

---
+++ submit_twitter.bat
@@ -0,0 +1,3 @@
+@echo off
+REM Submit the Spark job to the spark-master container
+docker exec sentiment-stream-spark-master-1 spark-submit --master spark://spark-master:7077 --deploy-mode client --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0 /opt/bitnami/spark/jobs/twitter_job.py

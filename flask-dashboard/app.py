ec66ddbce87b9b9ebb1d506ec0daf4920b020792
adil <adilsaid64@gmail.com>
2024-11-23 19:25:41 +0000

    feat: added container for flask

--- flask-dashboard/app.py
+++ flask-dashboard/app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
 @app.route('/api/data')
 def get_data():
      data = {
@@ -17,6 +16,5 @@ def get_data():
     }
      return jsonify(data)
 
-
 if __name__  == "__main__":
-     app.run(debug=True)
+     app.run(host="0.0.0.0", port=5000, debug=True)

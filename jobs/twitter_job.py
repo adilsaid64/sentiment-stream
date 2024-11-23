5a460004b5f24ae3c20e6c5a97ecfa459613bd71
adil <adilsaid64@gmail.com>
2024-11-23 15:44:03 +0000

    feat: both twitter job and spark job run in parallel. Data stored into S3 (minio).

--- jobs/twitter_job.py
+++ jobs/twitter_job.py
@@ -2,7 +2,7 @@
 docker exec sentiment-stream-spark-master-1 spark-submit --master spark://spark-master:7077 --deploy-mode client --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0 /opt/bitnami/spark/jobs/twitter_job.py
 """
 
-from jobs.src.spark_utils import logger, create_spark_session, get_twitter_schema, read_kafka_stream, process_batch
+from jobs.src.spark_utils import logger, create_spark_session, get_twitter_schema, read_kafka_stream, process_twitter_batch
 from pyspark.sql.types import StructType
 from pyspark.sql import SparkSession, DataFrame
 
@@ -17,7 +17,7 @@ def main() -> None:
     kafka_stream: DataFrame = read_kafka_stream(spark, "twitter", twitter_schema)
 
     logger.info("Starting stream processing...")
-    query = kafka_stream.writeStream.foreachBatch(process_batch).start()
+    query = kafka_stream.writeStream.foreachBatch(process_twitter_batch).start()
 
     try:
         query.awaitTermination()

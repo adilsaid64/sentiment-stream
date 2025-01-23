ab820def59168fff0f0ab649127e1333021d6aba
adil <adilsaid64@gmail.com>
2025-01-23 17:34:23 +0000

    chore: setup kafka init container to create kafka topics

---
+++ init-kafka/init-kafka.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Waiting for Kafka to be ready..."
+sleep 10
+
+kafka-topics \
+    --bootstrap-server broker:9092 \
+    --create \
+    --if-not-exists \
+    --topic reddit \
+    --replication-factor 1 \
+    --partitions 2 \
+    --config cleanup.policy=delete \
+    --config retention.ms=604800000
+
+
+kafka-topics \
+    --bootstrap-server broker:9092 \
+    --create \
+    --if-not-exists \
+    --topic twitter \
+    --replication-factor 1 \
+    --partitions 2 \
+    --config cleanup.policy=delete \
+    --config retention.ms=604800000
+
+
+kafka-topics \
+    --bootstrap-server broker:9092 \
+    --create \
+    --if-not-exists \
+    --topic processed-data \
+    --replication-factor 1 \
+    --partitions 2 \
+    --config cleanup.policy=delete \
+    --config retention.ms=604800000
+
+echo "Successfully created topics:"
+
+kafka-topics --bootstrap-server broker:9092 --list

8918865322a79f1193a3978e30f85144cf1e2c62
Adil Said <adilsaid64@gmail.com>
2025-01-29 20:56:11 +0000

    feat: added extract method, that extracts and aggregates data

--- etl-data-aggregator/main.py
+++ etl-data-aggregator/main.py
@@ -1,10 +1,34 @@
+import os
+from typing import Optional, Dict, List
 import time
 
+from s3_fetcher import S3Fetcher
+from context import  DataFetcher
+
+from dotenv import load_dotenv
+import boto3
+
 if __name__ == "__main__":
-    print("RUNNING ETL MOB JOB!")
+    load_dotenv()
 
-    print("...")
+    MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "http://localhost:9000")
+    MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+    MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
 
-    time.sleep(5)
 
-    print("ETL JOB COMPLETED!")
+    BUCKET_NAME : str = ""
+    PREFIX : Optional[str] = None
+
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=MINIO_ENDPOINT,
+        aws_access_key_id=MINIO_ACCESS_KEY,
+        aws_secret_access_key=MINIO_SECRET_KEY)
+    
+    s3_fetcher = DataFetcher(S3Fetcher(s3_client = s3_client,
+                           bucket_name = BUCKET_NAME,
+                           prefix = PREFIX))
+    
+    output : List[Dict[str, str]] = s3_fetcher.fetch()
+
+

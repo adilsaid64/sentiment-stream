9d21d03944dfb73cbfc023d03c786e7ab07a2316
Adil Said <adilsaid64@gmail.com>
2025-01-29 21:03:43 +0000

    chore: added a type dict fot twitter data

--- jobs/src/twitter_fetcher.py
+++ jobs/src/twitter_fetcher.py
@@ -6,11 +6,20 @@ import uuid
 # This is to provide an illustration of how we could have multiple data sources.
 from src.proj_logger import logger
 
+from typing import TypedDict
+
+
+class TwitterData(TypedDict):
+    id: str
+    username: str
+    comment: str
+    time: float
+
 class TwitterFetcher(FetchStrategy):
     def __init__(self, df:pd.DataFrame):
-        self.df = df.sample(frac =1) # this shuffles the data
+        self.df : pd.DataFrame = df.sample(frac =1) # this shuffles the data
         
-    def fetch_data(self):
+    def fetch_data(self)->TwitterData:
         while True:   
             for index, row in self.df.iterrows():
                 yield {

55ed930ff0fe09b08586f27248c77397127cd536
adil <adilsaid64@gmail.com>
2025-01-30 01:21:53 +0000

    feat:: convert to pandas dataframe to prep to save as csv

--- etl-data-aggregator/main.py
+++ etl-data-aggregator/main.py
@@ -8,6 +8,8 @@ from src.context import  DataFetcher
 from dotenv import load_dotenv
 import boto3
 
+import pandas as pd
+
 if __name__ == "__main__":
     load_dotenv()
 
@@ -16,8 +18,12 @@ if __name__ == "__main__":
     MINIO_SECRET_KEY : str = os.getenv("MINIO_SECRET_KEY")
 
 
-    BUCKET_NAME : str = "processed-data"
-    PREFIX : Optional[str] = "output/json_data"
+    SOURCE_BUCKET : str = "processed-data"
+    SOURCE_PREFIX : Optional[str] = "output/json_data"
+
+
+    DESTINATION_BUCKET : str = ""
+    DESTINATION_KEY : str = ""
 
     s3_client = boto3.client(
         "s3",
@@ -26,10 +32,15 @@ if __name__ == "__main__":
         aws_secret_access_key=MINIO_SECRET_KEY)
     
     s3_fetcher = DataFetcher(S3Fetcher(s3_client = s3_client,
-                           bucket_name = BUCKET_NAME,
-                           prefix = PREFIX))
+                           bucket_name = SOURCE_BUCKET,
+                           prefix = SOURCE_PREFIX))
     
     output : List[Dict[str, str]] = s3_fetcher.fetch()
 
 
-    print(output)
+    # print(output)
+
+
+    print(pd.DataFrame(output))
+
+    # store into s3 as csv file

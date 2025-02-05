d32c6b295592e8061211eabb61165c2a560a2556
adil <adilsaid64@gmail.com>
2025-02-05 00:16:40 +0000

    feat: added type hinting

--- project-core/src/my_package/kafka_consumer.py
+++ project-core/src/my_package/kafka_consumer.py
@@ -1,13 +1,13 @@
 from kafka import KafkaConsumer
 from interface import MessageConsumerStrategy
-
+from typing import Generator
 class KafkaConsumerStrategy(MessageConsumerStrategy):
     def __init__(self, bootstrap_servers : str, topic : str, group_id:str):
-        self.consumer = KafkaConsumer(topic, bootstrap_servers = bootstrap_servers, group_id = group_id)
+        self.consumer : KafkaConsumer = KafkaConsumer(topic, bootstrap_servers = bootstrap_servers, group_id = group_id)
 
-    def consume_message(self):
+    def consume_message(self) -> Generator[bytes, None, None]:
         for message in self.consumer:
             yield message.value
 
-    def close(self):
+    def close(self) -> None:
         self.consumer.close()

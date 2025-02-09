63a04d15c37fe4825e2348476e71c2275f0cf262
adil <adilsaid64@gmail.com>
2025-02-09 23:10:37 +0000

    feat: connect to local dataset

--- project-core/run.py
+++ project-core/run.py
@@ -2,8 +2,9 @@ from src.my_package.mlworkflow import TextBlobLabeling, BasicTextProcessor, Basi
 from src.my_package.mlpipeline import SentimentAnalysisPipeline
 import pandas as pd 
 
-data : pd.DataFrame = ... # connect to some data
-
+data : pd.DataFrame = pd.read_csv("../twitter-streamer/data/twitter_dataset.csv").rename(columns = {"Text":"text"}) # connect to some data
+print(data)
+data = pd.DataFrame(data['text'])
 pipeline = SentimentAnalysisPipeline(
     labeling_strategy=TextBlobLabeling(),
     processing_strategy=BasicTextProcessor(),
@@ -13,4 +14,5 @@ pipeline = SentimentAnalysisPipeline(
 )
 
 results = pipeline.run(data)
+
 print("Evaluation Results:", results)
